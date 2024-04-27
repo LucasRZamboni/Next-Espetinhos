98fedcfecfee3487c29141e7ae76bddac122bd61
LucasRZamboni <lucasrzamboni@hotmail.com>
2024-04-27 12:00:36 -0300

    Estilizando componente produto com next image

---
+++ src/app/components/ProductImage.tsx
@@ -0,0 +1,33 @@
+'use client'
+import { useState } from "react";
+import Image from "next/image";
+import { ProductType } from "../types/ProductType";
+
+type ProdutImageProps = {
+    product: ProductType;
+    fill?: boolean;
+}
+export default function ProductImage({ product, fill }: ProdutImageProps) {
+    const [loading, setLoading] = useState(true);
+
+    return fill ? (
+        <Image
+            src={product.image}
+            fill
+            alt={product.title}
+            className={`object-cover ${loading ? 'scale-110 blur-3xl grayscale' : 'scale-100 blur-0 grayscale-0'
+                }`}
+            onLoadingComplete={() => setLoading(false)}
+        />
+    ) : (
+        <Image
+            src={product.image}
+            width={400}
+            height={400}
+            alt={product.title}
+            className={`object-cover ${loading ? 'scale-110 blur-3xl grayscale' : 'scale-100 blur-0 grayscale-0'
+                }`}
+            onLoadingComplete={() => setLoading(false)}
+        />
+    );
+}
